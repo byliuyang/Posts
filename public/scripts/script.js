f2e310612edd5de0f6205af956cfb10f62a4e017
Yang Liu <byliuyang11@gmail.com>
2016-12-12 20:51:58 -0500

    Change google geocoding API key

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -58,7 +58,7 @@ function initEventHandler() {
 }
 
 function initStreaming() {
-    connection = new WebSocket('ws://localhost:8081/message');
+    connection = new WebSocket('ws://cs4241-a7-byliuyang.herokuapp.com:8081/message');
     connection.onopen = function () {
         connection.send('Ping');
     };
